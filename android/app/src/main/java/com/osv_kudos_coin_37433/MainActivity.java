25e0118c4899f60d38fd59681b09ad6c22c3c331
Crowdbotics <team@crowdbotics.com>
2022-10-31 13:02:47 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/osv_kudos_coin_37433/MainActivity.java
@@ -0,0 +1,15 @@
+package com.osv_kudos_coin_37433;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "osv_kudos_coin_37433";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "osv_kudos_coin_37433",
+  url: "https://osv_kudos_coin_37433.botics.co",
+  api: "https://osv_kudos_coin_37433.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
